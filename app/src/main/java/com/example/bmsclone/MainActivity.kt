bb320396d416e8387d431b5a09f2f4626b93286a
Mudra Bhandari <mudrabhandari123@yahoo.in>
2020-10-18 13:35:31 +0530

    Inital Commit

---
+++ app/src/main/java/com/example/bmsclone/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.bmsclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.DefaultItemAnimator
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showMovies(Movies.list!!)
+    }
+
+    private fun showMovies(movies: ArrayList<Movie>) {
+        recyclerView.visibility = View.VISIBLE
+        progressBar.visibility = View.GONE
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = MoviesAdapter(movies)
+        recyclerView.itemAnimator= DefaultItemAnimator()
+    }
+}
